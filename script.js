9eb2cd5f9794d6ddd3274f0de979224e53ba4c9a
kewinpyza <kewin.pyza@gmail.com>
2022-11-23 13:37:15 +0100

    Add styling with Bulma

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ btnPlayer1.addEventListener("click", function () {
     score1++;
     if (score1 === gamePoints) {
       gameOver = true;
-      scorePlayer1.classList.add("winner");
-      scorePlayer2.classList.add("loser");
+      scorePlayer1.classList.add("has-text-success");
+      scorePlayer2.classList.add("has-text-danger");
+      btnPlayer1.disabled = true;
+      btnPlayer2.disabled = true;
     }
     scorePlayer1.textContent = score1;
   }
@@ -27,8 +29,10 @@ btnPlayer2.addEventListener("click", function () {
     score2++;
     if (score2 === gamePoints) {
       gameOver = true;
-      scorePlayer2.classList.add("winner");
-      scorePlayer1.classList.add("loser");
+      scorePlayer2.classList.add("has-text-success");
+      scorePlayer1.classList.add("has-text-danger");
+      btnPlayer1.disabled = true;
+      btnPlayer2.disabled = true;
     }
     scorePlayer2.textContent = score2;
   }
@@ -47,6 +51,8 @@ function init() {
   score2 = 0;
   scorePlayer1.textContent = 0;
   scorePlayer2.textContent = 0;
-  scorePlayer1.classList.remove("winner", "loser");
-  scorePlayer2.classList.remove("winner", "loser");
+  scorePlayer1.classList.remove("has-text-success", "has-text-danger");
+  scorePlayer2.classList.remove("has-text-success", "has-text-danger");
+  btnPlayer1.disabled = false;
+  btnPlayer2.disabled = false;
 }
