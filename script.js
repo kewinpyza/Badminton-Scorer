17b3147730a2b315ed568177d69de4ef40599a64
kewinpyza <kewin.pyza@gmail.com>
2022-11-24 02:00:24 +0100

    Add some features

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
 const player1 = {
+  gameScore: 0,
   score: 0,
   button: document.querySelector(".player1-btn"),
+  displayGameScore: document.querySelector(".player1-game-score"),
   displayScore: document.querySelector(".player1-score"),
 };
 
 const player2 = {
+  gameScore: 0,
   score: 0,
   button: document.querySelector(".player2-btn"),
+  displayGameScore: document.querySelector(".player2-game-score"),
   displayScore: document.querySelector(".player2-score"),
 };
 
 const players = [player1, player2];
 
+const displaySet = document.querySelector(".game");
+const newSetBtn = document.querySelector(".set-btn");
 const resetBtn = document.querySelector(".reset-btn");
-const gamePointsSelect = document.querySelector("#playTo");
+const setPointsSelect = document.querySelector("#playto");
+const numSetsSelect = document.querySelector("#num-sets");
 
-let gamePoints = 9;
+let gameSet = 1;
+let numSets = 3;
+let setPoints = 9;
+let setOver = false;
 let gameOver = false;
 
 function scoreUpdate(player, rival) {
-  if (!gameOver) {
+  if (!setOver && !gameOver) {
     player.score++;
-    if (player.score === gamePoints) {
-      gameOver = true;
+    if (gameSet === 1 && (player.score === 1 || rival.score === 1)) {
+      setPointsSelect.disabled = true;
+      numSetsSelect.disabled = true;
+    }
+    if (player.score === setPoints) {
+      setOver = true;
       player.displayScore.classList.add("has-text-success");
       rival.displayScore.classList.add("has-text-danger");
       player.button.disabled = true;
       rival.button.disabled = true;
+      player.gameScore++;
+      player.displayGameScore.textContent = player.gameScore;
+    }
+    if (gameSet === numSets && setPoints === player.score) {
+      gameOver = true;
     }
     player.displayScore.textContent = player.score;
+  }
+  if (gameOver) {
+    newSetBtn.disabled = true;
   }
 }
 
@@ -40,15 +62,40 @@ player2.button.addEventListener("click", function () {
   scoreUpdate(player2, player1);
 });
 
-gamePointsSelect.addEventListener("change", function () {
+setPointsSelect.addEventListener("change", function () {
   init();
-  gamePoints = parseInt(this.value);
+  setPoints = parseInt(this.value);
 });
 
-resetBtn.addEventListener("click", init);
+numSetsSelect.addEventListener("change", function () {
+  init();
+  numSets = parseInt(this.value);
+});
+
+newSetBtn.addEventListener("click", function () {
+  if (setOver && gameSet < numSets) {
+    init();
+    gameSet++;
+    displaySet.textContent = gameSet;
+  }
+});
+
+resetBtn.addEventListener("click", function () {
+  init();
+  gameSet = 1;
+  gameOver = false;
+  newSetBtn.disabled = false;
+  displaySet.textContent = 1;
+  setPointsSelect.disabled = false;
+  numSetsSelect.disabled = false;
+  players.forEach((p) => {
+    p.displayGameScore.textContent = 0;
+    p.gameScore = 0;
+  });
+});
 
 function init() {
-  gameOver = false;
+  setOver = false;
   for (const p of players) {
     p.score = 0;
     p.displayScore.textContent = 0;
